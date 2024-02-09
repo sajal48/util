7df8529418b0f5aee79098db63cc47ffaed973e1
os-sajal.halder <os-sajal.halder@rakuten.com>
2024-02-09 15:23:52 +0900

    init

---
+++ src/main/java/org/example/CheckCache.java
@@ -0,0 +1,131 @@
+package org.example;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CheckCache {
+    // Set the root directory
+    public static String rootDir = "C:\\Users\\os-sajal.halder\\Desktop\\ECSG\\japan.web.gateway\\src\\main\\" +
+            "resources\\reloadable.config";
+    // Set the checkFile name
+    public static String checkFileName = "settings-prod.json";
+
+    public static void main(String[] args) {
+
+
+        // Lists to store enabled and disabled directories
+        List<String> enabledDirectories = new ArrayList<>();
+        List<String> disabledDirectories = new ArrayList<>();
+
+        // Create an ExecutorService with a fixed number of threads
+        int numThreads = Runtime.getRuntime().availableProcessors(); // Use the number of available processors
+        try (ExecutorService executorService = Executors.newThreadPerTaskExecutor(
+                Thread.ofVirtual().name("cache-enable-checker-vt", 0).factory())) {
+
+            // List to store Future tasks
+            List<Future<?>> futures = new ArrayList<>();
+
+            // Loop through directories
+            for (String dir : Objects.requireNonNull(new File(rootDir).list())) {
+                String jsonFilePath = rootDir + "\\" + dir + "\\" + checkFileName;
+
+                // Submit each directory for processing
+                futures.add(executorService.submit(
+                        () -> processDirectory(jsonFilePath, dir, enabledDirectories, disabledDirectories)));
+            }
+
+            // Wait for all tasks to complete
+            for (Future<?> future : futures) {
+                try {
+                    future.get();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // Shut down the ExecutorService
+            executorService.shutdown();
+        }
+
+        // Display the results
+        printDirectories("Enabled", enabledDirectories);
+//        printDirectories("Disabled", disabledDirectories);
+    }
+
+    private static void processDirectory(String jsonFilePath, String dir, List<String> enabledDirectories,
+                                         List<String> disabledDirectories) {
+        // Check if settings-prod.json exists in the current directory
+        if (new File(jsonFilePath).exists()) {
+            try {
+                if (isCacheEnabled(jsonFilePath)) {
+                    if(!hasCacheKeyForAllParameter(jsonFilePath)){
+                        enabledDirectories.add(dir);
+                    };
+                } else {
+                    disabledDirectories.add(dir);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private static JSONObject getJsonObject(String jsonFilePath) throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader(jsonFilePath));
+        return (JSONObject) obj;
+    }
+
+    private static void printDirectories(String status, List<String> directories) {
+        System.out.println("\n" + status + " Directories:");
+        for (String dir : directories) {
+            System.out.println(dir);
+        }
+    }
+
+    private static boolean isCacheEnabled(String jsonFilePath) throws IOException, ParseException {
+        JSONObject jsonObject = getJsonObject(jsonFilePath);
+        Object cache = jsonObject.get("cache");
+        return cache instanceof JSONObject &&
+                "true".equalsIgnoreCase(((JSONObject) cache).get("enabled").toString());
+    }
+
+    private static boolean hasCacheKeyForAllParameter(String directoryPath) throws IOException, ParseException {
+        String schemaFilePath = directoryPath + "\\..\\schema.json";
+        if (new File(schemaFilePath).exists()) {
+            JSONObject jsonObject = getJsonObject(schemaFilePath);
+            JSONObject requestObj = (JSONObject) jsonObject.get("request");
+            return checkCacheKeyParameters(requestObj,"urlParameters") &&
+                    checkCacheKeyParameters(requestObj,"metaParameters") &&
+                    checkCacheKeyParameters(requestObj,"bodyParameters");
+        }
+        return false;
+    }
+    private static boolean checkCacheKeyParameters(JSONObject requestObj,String parameterName) {
+        JSONArray jsonArray = (JSONArray) requestObj.get(parameterName);
+        if(jsonArray == null || jsonArray.isEmpty()){
+            return true;
+        }
+        for (Object obj : jsonArray) {
+            if (obj instanceof JSONObject) {
+                Object cacheKeyObj = ((JSONObject) obj).get("cacheKey");
+                if (cacheKeyObj == null) {
+                    return false;
+                }
+            }
+        }
+        return true; // All objects have "cacheKey"
+    }
+}
+
